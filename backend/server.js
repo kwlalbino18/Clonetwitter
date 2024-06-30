cb63af50ab587ee95111f4ca462d0e834990a82c
kwlalbino18 <kwlalbino18@gmail.com>
2024-06-30 11:51:09 -0400

    listo

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import {v2 as cloudinary} from "cloudinary";
@@ -25,7 +26,7 @@ cloudinary.config({
 
 const app=express();
 const port=process.env.PORT || 5000;
-
+const __dirname= path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
@@ -37,6 +38,15 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
     connectMongoDB();
